8f674646952a8d5e2f82bb08bb778a3dc7ffa191
23divyajain12-afk <23divyajain12@gmail.com>
2026-08-07 22:03:31 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.97%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum =0, max_sum= INT_MIN;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum+=nums[i];
+            max_sum = max(max_sum, sum);
+            sum = max(sum,0);
+
+        }
+        return max_sum;
+    }
+};
