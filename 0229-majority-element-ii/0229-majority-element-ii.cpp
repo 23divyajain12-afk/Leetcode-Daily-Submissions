f090efc237a41107edab455b8541feac5395042c
23divyajain12-afk <23divyajain12@gmail.com>
2026-08-31 20:55:15 +0530

    Time: 0 ms (100%), Space: 23.8 MB (48.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> hash;
-        vector<int> ans={};
-        for(int i=0;i<nums.size();i++){
-            hash[nums[i]]++;
-            if(hash[nums[i]]==nums.size()/3+1) ans.push_back(nums[i]);
+        int cnt1=0, cnt2=0;
+    int el1=INT_MIN, el2=INT_MIN;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if(cnt1==0 && nums[i]!=el2){
+            cnt1=1;
+            el1=nums[i];
         }
-        return ans;
+        else if(cnt2==0 && nums[i]!=el1){
+            cnt2=1;
+            el2=nums[i];
+        }
+        else if(nums[i]==el1) cnt1++;
+        else if(nums[i]==el2) cnt2++;
+        else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+    cnt1=0;
+    cnt2=0;
+    for(int i:nums){
+        if(i==el1) cnt1++;
+        if(i==el2) cnt2++; 
+    }
+    if(cnt1>nums.size()/3) ans.push_back(el1); 
+    if(cnt2>nums.size()/3) ans.push_back(el2);
+    return ans;
     }
 };
