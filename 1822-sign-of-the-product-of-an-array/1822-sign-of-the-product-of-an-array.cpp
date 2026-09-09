91eb5b71a1d0dbda338ee0081f15d2271cb85c8b
23divyajain12-afk <23divyajain12@gmail.com>
2026-09-09 18:51:56 +0530

    Time: 0 ms (100%), Space: 13.7 MB (79.27%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int neg =0;
+        int neg=0;
         for(int i:nums){
             if(!i) return 0;
             if(i<0) neg++;
         }
-        return(neg%2==0)?1:-1;
+        return (neg%2==0)? 1:-1;
     }
 };
