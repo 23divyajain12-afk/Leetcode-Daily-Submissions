ffc3772b144520bb8a2e33a20bee5f60c63c4a92
23divyajain12-afk <23divyajain12@gmail.com>
2026-09-10 23:53:18 +0530

    Time: 47 ms (62.62%), Space: 29 MB (72.96%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    int i = 0;
+    int n = nums.size();
+    while (i < n - 2)
+    {
+        if (i != 0 && nums[i] == nums[i - 1])
+        {
+            i++;
+        }
+        else
+        {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                    {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1])
+                    {
+                        k--;
+                    }
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+            i++;
+        }
+    }
+    return ans;
+    }
+};
