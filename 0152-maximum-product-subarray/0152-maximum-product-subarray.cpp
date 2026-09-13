2b2aa5e5dc4c9a26a17cb9d1b5ffb72a7c17a6e6
23divyajain12-afk <23divyajain12@gmail.com>
2026-09-14 01:43:50 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.27%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxi = nums[0];
-        for(int i=0;i<nums.size();i++){
-            int cur = 1;
-            for(int j=i;j<nums.size();j++){
-                cur *= nums[j];
-                maxi = max(cur, maxi);
-            }
+        int maxi = INT_MIN;
+        int pre = 1, suf = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            pre *= nums[i];
+            suf *= nums[nums.size() - i - 1];
+            maxi = max(maxi, max(pre, suf));
+            if (pre == 0)
+                pre = 1;
+            if (suf == 0)
+                suf = 1;
         }
         return maxi;
     }
