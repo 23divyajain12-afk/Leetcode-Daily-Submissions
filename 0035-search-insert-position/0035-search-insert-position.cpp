dbfa2b1532b20ab1e6bdb590aaa3d069406ea327
23divyajain12-afk <23divyajain12@gmail.com>
2026-09-14 17:14:42 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.2%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0, high = nums.size()-1;
+    int lb = nums.size();
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            lb = min(mid, lb);
+            high = mid-1;
+        } 
+        else{
+            low=mid+1;
+        }
+    }
+    return lb;
+    }
+};
