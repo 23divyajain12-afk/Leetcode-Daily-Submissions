d6eb169c640d69277c6ad5caa546f5e8888e6d69
23divyajain12-afk <23divyajain12@gmail.com>
2026-09-23 00:11:02 +0530

    Time: 2 ms (15.2%), Space: 8.8 MB (88.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (char ch : s) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                st.push(ch);
+        for (char i : s) {
+            if (i == '(' || i == '[' || i == '{') {
+                st.push(i);
             } else {
                 if (st.empty()) {
                     return false;
                 }
                 char top = st.top();
-                st.pop();
-                if (ch == ')' && top != '(') return false;
-                if (ch == ']' && top != '[') return false;
-                if (ch == '}' && top != '{') return false;
+                if ((top == '(' && i == ')') || (top == '[' && i == ']') ||
+                    (top == '{' && i == '}')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
             }
         }
         return st.empty();
